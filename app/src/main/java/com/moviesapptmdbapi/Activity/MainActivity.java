4c80dcf9b5a15f19c8242986bc1daae69a1a86f1
leo827 <78188180+leo827@users.noreply.github.com>
2021-08-01 10:38:49 -0500

    Update MainActivity.java

--- app/src/main/java/com/moviesapptmdbapi/Activity/MainActivity.java
+++ app/src/main/java/com/moviesapptmdbapi/Activity/MainActivity.java
@@ -191,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showError() {
-        Toast.makeText(MainActivity.this, "Por favor revise .", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Por favor revise su conexion a internet .", Toast.LENGTH_LONG).show();
     }
 
 }
